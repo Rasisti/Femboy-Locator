ef58677c74ef23392f93ccdc6389a3dc6bda2bfc
Rasisti <masakissa06@gmail.com>
2025-02-27 20:43:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -6,12 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files (for your HTML, CSS, and JS)
+// Serve static files (HTML, CSS, JS) from the 'public' directory
 app.use(express.static('public'));
 
 // When a user connects, listen for their location updates
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('A user connected');
 
   // When a user sends their location, broadcast it to everyone else
   socket.on('location', (data) => {
@@ -20,11 +21,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('User disconnected');
   });
 });
 
 // Listen on port 3000
 server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server running on http://localhost:3000');
 });
